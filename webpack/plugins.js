e3cc9d1c389fc9a6d240b6a0404a0083f7a71e1e
Viseko <viseko23@gmail.com>
2022-08-25 04:05:24 +0700

    Added stylelint rules

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,13 +1,22 @@
 const PugPlugin = require("pug-plugin");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
 
-module.exports = env => {
+module.exports = () => {
   const plugins = [
     new PugPlugin({
       pretty: true,
       extractCss: {
         filename: "styles/[name].[contenthash:8].css",
       },
-    })
+    }),
+    new StyleLintPlugin({
+      configFile: ".stylelintrc.json",
+      context: "src",
+      files: "**/*.scss",
+      fix: true,
+      failOnError: false,
+      quiet: true,
+    }),
   ];
 
   return plugins;
