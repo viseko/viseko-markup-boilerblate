76b8e5690297d0e9d52557fed82f06d4a226220c
Viseko <viseko23@gmail.com>
2022-08-25 08:42:41 +0700

    Added temporary code in SRC-files

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
-const doSomething = (el) => {
-  console.log(el)
-};
+const arr = [];
 
-const arr = [1, 2, 3, 5];
-
-const myName = "Ivan";
-
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i].name === myName) {
-      doSomething(arr[i]);
-      // break was supposed to be here
-  }
-  break;
+for (let i = 0; i < 20; i++) {
+  arr.push(i);
 }
